f9e2161d6a751c3ce16a7c9a973a3bb63d3b4496
mey yaou <meynedjmadehbiyayaou@gmail.com>
2026-06-24 21:39:49 +0100

    Dico in english

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const search = document.getElementById("search") 
+const handleClick= async() =>{
+  const input = document.querySelector("#input").value; 
+  if (!input || input==""){
+    alert("Please enter a valid input");
+    return;
+  }
+  document.querySelector(".fa.fa-spinner.fa-spin").style.display="block";
+  //document.querySelector(".fa.fa-search").style.display= "none";
+  console.log(input);
+  getWordDefinition(input);
+ 
+
+
 const searchBox = document.getElementsByClassName("search-box") 
 const listResult = document.getElementsByClassName("list-result") 
 const wordTxt = document.getElementsByClassName("word-txt") 
@@ -6,7 +18,27 @@ const definitionTxt = document.getElementsByClassName("defintion-txt")
 const errorTxt = document.getElementsByClassName("error") 
 const resultsBox= document.querySelector(".result-box")
 
-async function getWordDefinition(word){
-  const res= "definition here" // await fetch(appel à api)
+
 }
 
+async function getWordDefinition(word){// await fetch(appel à api)
+  const url= "https://api.dictionaryapi.dev/api/v2/entries/en/"+ word;
+  try{
+    const response = await fetch(url);
+    const result = await response.text();
+    const responseObj= JSON.parse(result);
+    document.querySelector(".fa.fa-spinner.fa-spin").style.display="none";
+    document.querySelector(".result-box").style.display="block";
+    document.querySelector("#word-txt").innerText=word;
+    document.querySelector(".definition-txt").innerText=responseObj[0].meanings[0].definitions[0].definition;
+    document.querySelector("#phonetic-txt").innerText=responseObj[0].phonetic;
+    document.querySelector("#type-txt").innerText=responseObj[0].meanings[0].partOfSpeech;
+
+  } catch (error){
+    console.log(error);
+    document.querySelector(".error").innerText=error;
+    //document.querySelector("#word-text").style.display="none";
+    //document.querySelector(".definition-txt").style.display="none";
+  }
+   
+}
